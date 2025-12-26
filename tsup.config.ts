ed9cd4183aa3433484c98e1d52cd9329ebc1506a
halilatilla <atillahalil0@gmail.com>
2025-12-27 00:17:11 +0300

    fix: reduce package size by 66% (remove sourcemaps, add minification)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
-  sourcemap: true,
+  sourcemap: false, // Removed to reduce package size
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
+  minify: true, // Minify to further reduce size
 });
 
